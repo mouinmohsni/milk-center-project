df39a17e9a943384b8567004f8512fbfeebd2dfd
mouin <mouin.mohsni@gmail.com>
2026-08-12 00:53:57 +0100

    link with identity - DiscoveryServer -test ok

--- identity-service/src/main/java/org/milkcenter/identityservice/IdentityServiceApplication.java
+++ identity-service/src/main/java/org/milkcenter/identityservice/IdentityServiceApplication.java
@@ -2,9 +2,11 @@ package org.milkcenter.identityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class IdentityServiceApplication {
 
     public static void main(String[] args) {
